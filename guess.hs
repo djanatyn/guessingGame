f54777abfa78676d768149665aecdf32e902f3eb
Jonathan Strickland <djanatyn@gmail.com>
2011-05-14 22:37:23 -0400

    first commit

---
+++ guess.hs
@@ -0,0 +1,33 @@
+import System.Random
+-- configuration :)
+numberOfAttempts = 7
+theAnswer = 24
+
+-- returns whether the guess is high or low
+result :: Int -> String
+result n
+    | n > theAnswer = "You're a bit high.\n"
+    | n < theAnswer = "You're a bit low.\n"
+    | otherwise     = "...you broke the game! :(\n"
+-- if you have no lives left
+prompt 0 = youLose
+-- if you have lives left
+prompt lives = do
+    putStrLn ("You've got " ++ (show lives) ++ " lives left.")
+    putStrLn "Enter a number from 1-100 >"
+    guessedNumber <- getLine
+    if (read guessedNumber :: Int) == theAnswer
+    then youWin
+    else do
+        putStrLn $ result (read guessedNumber :: Int)
+        prompt (lives - 1)
+
+-- if you win
+youWin = putStrLn "\nYou win!"
+-- if you lose
+youLose = putStrLn "\nYou lose."
+
+main = do
+    putStrLn "djanatyn's guessing game"
+    putStrLn "------------------------\n"
+    prompt numberOfAttempts
